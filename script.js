f4d83271de3c5904f7dcb86f134daafed2193765
RheaR12 <98754117+RheaR12@users.noreply.github.com>
2025-12-01 12:59:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const mobileBtn = document.querySelector('.mobile-menu-btn');
+const mobileNav = document.querySelector('.mobile-nav');
+const closeBtn = document.querySelector('.close-mobile-nav');
+
+mobileBtn.addEventListener('click', () => {
+  mobileNav.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+  mobileNav.classList.remove('open');
+});
 
